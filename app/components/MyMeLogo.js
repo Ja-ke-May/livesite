37315bac002c74ae1743e6f286e4fff87fe0e337
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-07-16 01:42:53 +0100

    voter animations no pointer events

--- app/components/MyMeLogo.js
+++ app/components/MyMeLogo.js
@@ -1,12 +1,10 @@
-// components/MyMeLogo.js
-
 import React from 'react';
 
 const MyMeLogo = ({ isDarkBackground }) => {
   const logoColor = isDarkBackground ? 'text-white' : 'text-[#000110]';
 
   return (
-    <div className={`fixed top-0 left-0 ${logoColor} flex items-center pt-1 pl-1 md:pt-2 md:pr-4 md:pb-4 md:pl-4`}>
+    <a href="/" className={`fixed top-0 left-0 ${logoColor} flex items-center pt-1 pl-1 md:pt-2 md:pr-4 md:pb-4 md:pl-4`}>
       <div className="flex items-center justify-center">
         <p className="text-5xl font-black">M</p>
       </div>
@@ -14,7 +12,7 @@ const MyMeLogo = ({ isDarkBackground }) => {
         <p className="text-xl font-black leading-none">Y</p>
         <p className="text-xl font-black leading-none">E</p>
       </div>
-    </div>
+    </a>
   );
 };
 
