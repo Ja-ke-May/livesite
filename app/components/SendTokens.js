d59f2c907e6cdcca1a3ced88cd3aa2b78dc35dcf
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-07-20 03:40:04 +0100

    pop up menu fixed

--- app/components/SendTokens.js
+++ app/components/SendTokens.js
@@ -2,28 +2,16 @@ import React, { useEffect, useRef, forwardRef, useImperativeHandle, useState } f
 
 const SendTokensPopUp = forwardRef(({ visible, onClose }, ref) => {
   const [tokenAmount, setTokenAmount] = useState(1);
-  const [isIncrementing, setIsIncrementing] = useState(false);
-  const [incrementInterval, setIncrementInterval] = useState(null);
   const popupRef = useRef(null);
 
   useImperativeHandle(ref, () => ({
     contains: (element) => popupRef.current.contains(element),
   }));
 
-  // Handle token amount increment
-  useEffect(() => {
-    if (isIncrementing) {
-      const intervalId = setInterval(() => {
-        setTokenAmount(prev => prev + 1);
-      }, 100);
-      setIncrementInterval(intervalId);
-    } else if (incrementInterval) {
-      clearInterval(incrementInterval);
-      setIncrementInterval(null);
-    }
-
-    return () => clearInterval(incrementInterval);
-  }, [isIncrementing]);
+  // Function to increment the token amount
+  const incrementTokenCount = () => {
+    setTokenAmount(prev => prev + 1);
+  };
 
   const handleSendTokens = () => {
     console.log(`Sending ${tokenAmount} tokens`);
@@ -52,17 +40,13 @@ const SendTokensPopUp = forwardRef(({ visible, onClose }, ref) => {
 
   return (
     <div ref={popupRef} className="relative absolute right-0 top-0 bg-gray-800 p-4 rounded-[5%] shadow-lg mt-2 max-w-md w-full">
-     
-      
       <h3 className="text-xl font-semibold mb-4 text-center text-yellow-400 brightness-125">Send Tokens</h3>
       
       <div className="relative w-full p-2 mb-4 rounded border border-blue-600 bg-gray-900 text-white text-center text-xl">
         <span>{tokenAmount}</span>
         <button
           className="absolute right-2 top-1/2 transform -translate-y-1/2 py-2 px-4 border border-blue-600 rounded-md shadow-sm text-sm font-medium text-white bg-gray-800/80 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700"
-          onMouseDown={() => setIsIncrementing(true)}
-          onMouseUp={() => setIsIncrementing(false)}
-          onMouseLeave={() => setIsIncrementing(false)}
+          onClick={incrementTokenCount}
         >
           +
         </button>
