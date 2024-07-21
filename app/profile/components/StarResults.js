29d0f61e78afec4c71ede8fbf4371895a5cd7cd3
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-07-21 23:52:40 +0100

    stars results profile

--- app/profile/components/StarResults.js
+++ app/profile/components/StarResults.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const ProfileStarResults = () => {
   return (
-    <div className="bg-gray-800/80 rounded-lg shadow-md p-6 mt-4">
+    <div className="bg-gray-800/80 rounded-lg shadow-md p-6 mt-4 flex flex-col items-center">
       <h3 className='text-4xl md:text-5xl font-semibold mb-4 text-center text-[#000110]'>Stars Results</h3>
         <h3>Longest Time Live</h3>
         <h3>Most Time Live</h3>
         <h3>Most Supporters</h3>
-        <h3>Top Commenters</h3> 
+        <h3>'First' comments</h3> 
+        <h3>'Last' comments</h3> 
       
     </div>
   );
