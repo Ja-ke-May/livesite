6f65ee096c4afca2d75ebd269d9b17753363f688
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-08-19 19:21:37 +0100

    added multiple stun servers fro production

--- app/components/viewer/Viewer.js
+++ app/components/viewer/Viewer.js
@@ -301,7 +301,10 @@ const Viewer = () => {
 
     const createPeerConnection = (id) => {
         const peerConnection = new RTCPeerConnection({
-            iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+            iceServers: [
+                { urls: "stun:stun.l.google.com:19302" },
+                { urls: "stun:stun2.l.google.com:19302" },
+            ],
         });
 
         peerConnection.onicecandidate = (event) => {
