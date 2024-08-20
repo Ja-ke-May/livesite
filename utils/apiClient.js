07e9c9714e034cf5fd99ac9bd51d1d289c6830f3
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-08-20 19:59:28 +0100

    put online

--- utils/apiClient.js
+++ utils/apiClient.js
@@ -2,7 +2,8 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://livesite-backend.onrender.com';
+// http://localhost:5000
 
 const getToken = () => localStorage.getItem('token');
 
