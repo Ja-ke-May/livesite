5b005b6d6db8fe373df42bc48966ad508ac749fb
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-08-25 03:32:37 +0100

    pic

--- app/profile/[username]/page.js
+++ app/profile/[username]/page.js
@@ -13,7 +13,7 @@ const LinksSection = lazy(() => import('../components/Links'));
 
 const ProfileContent = ({ profileUsername }) => {
   const { isLoggedIn, username: loggedInUsername, isInitialized } = useContext(AuthContext);
-  const [profilePicture, setProfilePicture] = useState("/images/logo.jpg");
+  const [profilePicture, setProfilePicture] = useState("");
   const [bio, setBio] = useState('');
   const [links, setLinks] = useState([]);
   const [tokens, setTokens] = useState(0);
@@ -33,7 +33,7 @@ const ProfileContent = ({ profileUsername }) => {
           fetchRecentActivity(profileUsername)
         ]);
 
-        setProfilePicture(userProfile.profilePicture || "/images/logo.jpg");
+        setProfilePicture(response.profilePicture ? `data:image/jpeg;base64,${response.profilePicture}` : '/images/logo.jpg');
         setBio(userProfile.bio || `Hi, I'm ${userProfile.userName}! Welcome to my profile 😊`);
         setLinks(userProfile.links || []);
         setTokens(userProfile.tokens || 0);
