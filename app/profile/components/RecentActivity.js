599166974b0c5ec279442a1fc8d14d788b66f8f9
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-09-01 03:22:23 +0100

    recent activity duplicates

--- app/profile/components/RecentActivity.js
+++ app/profile/components/RecentActivity.js
@@ -7,6 +7,11 @@ const RecentActivity = ({ recentActivity, usernameColor, commentColor, borderCol
     setVisibleCount((prevCount) => prevCount + 20);
   };
 
+  const uniqueActivities = recentActivity.filter((activity, index, self) => {
+    const activityTimestamp = activity.match(/\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2}:\d{2}/)[0];
+    return index === self.findIndex(a => a.includes(activityTimestamp));
+  });
+
   return (
     <div className="max-h-[400px] bg-gray-800/80 rounded-lg shadow-md p-6 mt-4"
     style={{ borderColor: borderColor, borderWidth: '2px', borderStyle: 'solid' }}
@@ -17,10 +22,10 @@ const RecentActivity = ({ recentActivity, usernameColor, commentColor, borderCol
       <ul className="list-disc pl-5 overflow-y-auto max-h-[300px] max-x-full"
       style={{ color: commentColor }}
       >
-        {[...recentActivity].reverse().slice(0, visibleCount).map((activity, index) => (
+          {[...uniqueActivities].reverse().slice(0, visibleCount).map((activity, index) => (
           <li key={index}>{activity}</li>
         ))}
-         {visibleCount < recentActivity.length && (
+         {visibleCount < uniqueActivities.length && (
         <button
           onClick={handleLoadMore}
           className="mt-4 px-4 py-2 bg-[#000110] text-white rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700"
