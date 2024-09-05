81db7c946cd3733ca953f2474287deae8ad0f0c2
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-09-05 07:48:32 +0100

    activate page

--- app/activate/page.js
+++ app/activate/page.js
@@ -1,6 +1,13 @@
 
+"use client";
+
+import React, { Suspense } from 'react';
 import ActivateAccount from './components/ActivateAccount';
 
 export default function ActivatePage() {
-    return <ActivateAccount />;
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <ActivateAccount />
+      </Suspense>
+    );
   }
