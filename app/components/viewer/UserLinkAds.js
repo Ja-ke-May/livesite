4e48f7f119f6323b729a8a0ab1578152182a6df5
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-09-08 22:37:02 +0100

    ads links

--- app/components/viewer/UserLinkAds.js
+++ app/components/viewer/UserLinkAds.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { fetchUserAds } from '@/utils/apiClient';
 
-// Utility function to check if a string is a valid base64 string
 const isBase64 = (str) => {
   if (typeof str !== 'string') {
     return false;
   }
   try {
-    const result = btoa(atob(str)) === str;
-    console.log('Valid base64:', result, str);
-    return result;
+    return btoa(atob(str)) === str;
   } catch (err) {
-    console.log('Invalid base64:', str);
     return false;
   }
 };
@@ -24,18 +20,21 @@ const UserLinkAds = () => {
     const fetchAds = async () => {
       try {
         const fetchedAds = await fetchUserAds();
-        console.log('Fetched Ads:', fetchedAds); // Log fetched ads
+        console.log('Fetched Ads:', fetchedAds); 
 
         const adsWithValidImages = fetchedAds.ads.map((ad, index) => {
           console.log(`Checking ad #${index}:`, ad);
 
-          if (!isBase64(ad.imageUrl)) {
+          const imageUrl = ad.links && ad.links.imageUrl ? ad.links.imageUrl : ad.imageUrl;
+
+          if (!isBase64(imageUrl)) {
             console.log(`Invalid imageUrl for ad #${index}, using fallback.`);
-            // If imageUrl is not in base64, use a fallback image
-            ad.imageUrl = '/images/logo.jpg'; // Fallback image
+            ad.imageUrl = '/images/logo.jpg'; 
           } else {
             console.log(`Valid base64 image for ad #${index}`);
+            ad.imageUrl = `data:image/png;base64,${imageUrl}`; 
           }
+
           return ad;
         });
 
@@ -53,10 +52,10 @@ const UserLinkAds = () => {
   const renderAd = (ad, index) => (
     <div className="w-full flex justify-center" key={index}>
       <div className={`ad-container ad-animation-${index} flex justify-center items-center`}>
-        <a href={ad.url} target="_blank" rel="noopener noreferrer">
+        <a href={ad.links?.url} target="_blank" rel="noopener noreferrer">
           <img
             src={ad.imageUrl.startsWith('data:image') ? ad.imageUrl : '/images/logo.jpg'}
-            alt={ad.text || 'Ad Image'} // Fallback alt text
+            alt={ad.links?.text || 'Ad Link Image'} 
             className="w-full h-full rounded"
           />
         </a>
