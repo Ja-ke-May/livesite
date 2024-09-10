6f8983235a0ac30331806bc027d022a7373aa44c
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-09-10 06:15:17 +0100

    removed special char from pass

--- app/login/components/SignUpForm.js
+++ app/login/components/SignUpForm.js
@@ -10,7 +10,6 @@ const validatePassword = (password) => {
     { regex: /[A-Z]/, message: 'Password must contain at least one uppercase letter.' },
     { regex: /[a-z]/, message: 'Password must contain at least one lowercase letter.' },
     { regex: /[0-9]/, message: 'Password must contain at least one number.' },
-    { regex: /[!@#$%^&*(),.?":{}|<>]/, message: 'Password must contain at least one special character.' },
   ];
 
   return requirements
