1e3de7ed04dbec506de16d06723f7d762b45b729
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-07-17 17:05:03 +0100

    brit games soon

--- app/components/viewer/Viewer.js
+++ app/components/viewer/Viewer.js
@@ -570,7 +570,7 @@ const Viewer = ( { isAdmin, isBlocked } ) => {
                 username={username} 
                 isBlocked={isBlocked}
             />
-            <div className="pt-1 h-8 bg-gradient-to-tr from-red-600 via-white to-blue-600 w-full font-bold text-md md:text-md text-center text-[#000110] brightness-125 rounded relative">
+            <div className="mb-2 pt-1 h-8 bg-gradient-to-tr from-red-600 via-white to-blue-600 w-full font-bold text-md md:text-md text-center text-[#000110] brightness-125 rounded relative">
                 BRITGAMES - Coming Soon...
             </div>
             <div className="group"> 
