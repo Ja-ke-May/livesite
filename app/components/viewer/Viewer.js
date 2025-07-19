7e94ec8ac8dbf0d4d22d2844fc91f39664920abc
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-07-19 22:59:38 +0100

    luxury votes

--- app/components/viewer/Viewer.js
+++ app/components/viewer/Viewer.js
@@ -570,9 +570,11 @@ const Viewer = ( { isAdmin, isBlocked } ) => {
                 username={username} 
                 isBlocked={isBlocked}
             />
-            <div className="mb-2 pt-1 h-8 bg-gradient-to-tr from-red-600 via-white to-blue-600 w-full font-bold text-md md:text-md text-center text-[#000110] brightness-125 rounded relative">
+            <button className="mb-2 pt-1 h-8 bg-gradient-to-tr from-red-600 via-white to-blue-600 w-full font-bold text-md md:text-md text-center text-[#000110] brightness-125 rounded relative"
+            onClick={() => (window.location.href = '/shop')}
+            >
                 BRITGAMES - Coming Soon...
-            </div>
+            </button>
             <div className="group"> 
                 <ViewerMain
                     mainVideoRef={mainVideoRef}
