922ffc73f5c8df8e41f306821f312a8f6190808d
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-07-21 00:22:58 +0100

    brit games shop

--- app/shop/components/BritGamesShop.js
+++ app/shop/components/BritGamesShop.js
@@ -172,7 +172,8 @@ useEffect(() => {
                 onClick={() =>
                   handlePurchaseClick("Brit Stick", 400, selectedItem.player)
                 }
-                disabled={isPurchasing || !selectedItem.player}
+                disabled
+                // ={isPurchasing || !selectedItem.player}
               >
                 Purchase
               </button>
@@ -198,7 +199,8 @@ useEffect(() => {
                   isPurchasing ? "animate-pulse" : ""
                 }`}
                 onClick={() => handlePurchaseClick("Safety Boat", 20000)}
-                disabled={isPurchasing}
+                disabled
+                // ={isPurchasing}
               >
                 Purchase
               </button>
@@ -306,8 +308,8 @@ useEffect(() => {
     aria-label="Vote up" 
       
 onClick={() => handleVoteRequest("Luxury Upvote", "upvote", 10000)}
-
-     disabled={activeIndex === 0}
+disabled
+    //={activeIndex === 0}
     >
       /\
     </button>
