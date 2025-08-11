7be8518385ed7d0388c1987f4e1d75b2808a9e89
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-11 04:57:31 +0100

    flag

--- app/shop/components/UsernameFlag.js
+++ app/shop/components/UsernameFlag.js
@@ -175,7 +175,7 @@ export default function FlagShop() {
           <button
             onClick={() => setShowPopup(true)} 
             disabled={loading}
-            className="bg-yellow-400 font-bold brightness-125 text-[#000110] py-2 rounded-md shadow-sm hover:bg-yellow-600 disabled:opacity-50 disabled:cursor-not-allowed transition-colors duration-200"
+            className={`mt-2 mb-5 bg-yellow-400 font-bold brightness-125 text-[#000110] px-2 py-1 rounded-md shadow-sm hover:bg-yellow-600`}
           >
             {loading ? "Processing..." : "Purchase"}
           </button> 
