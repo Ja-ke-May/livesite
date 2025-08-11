4823b414dd07751ca95dae12f3a87da811d628cf
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-11 03:45:21 +0100

    flag

--- app/profile/components/ProfileInfo.js
+++ app/profile/components/ProfileInfo.js
@@ -140,13 +140,7 @@ const ProfileInfo = ({
         />
       )}
             {username}
-             {flag && (
-        <img
-          src={`https://flagcdn.com/w40/${flag}.png`}
-          alt="flag"
-          className="h-5 w-8 object-cover rounded-sm"
-        />
-      )}
+            
             
           </h2>
 
