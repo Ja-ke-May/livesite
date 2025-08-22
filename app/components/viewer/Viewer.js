054911abb07b9f3e27b8d731df9a3d403562f5db
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-22 16:44:38 +0100

    britgames without buttons

--- app/components/viewer/Viewer.js
+++ app/components/viewer/Viewer.js
@@ -573,7 +573,7 @@ const Viewer = ( { isAdmin, isBlocked } ) => {
             <button className="mb-2 pt-1 h-8 bg-gradient-to-tr from-red-600 via-white to-blue-600 w-full font-bold text-md md:text-md text-center text-[#000110] brightness-125 rounded relative"
             onClick={() => (window.location.href = '/shop')}
             >
-                BRITGAMES - Coming Soon...
+                BRITGAMES - Aug 29-31
             </button>
             <div className="group"> 
                 <ViewerMain
