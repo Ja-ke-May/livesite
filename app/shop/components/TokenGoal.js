fe8292cd90618209d627940ed6da1e4350560cb6
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-22 16:47:03 +0100

    britgames without buttons

--- app/shop/components/TokenGoal.js
+++ app/shop/components/TokenGoal.js
@@ -49,7 +49,7 @@ const TokenGoal = ({ item, pot, goal, isPurchasing, isLoggedIn, username }) => {
   const progress = Math.min((currentTokens / goal) * 100, 100);
 
   return (
-    <div className="w-full mt-4">
+    <div className="hidden w-full mt-4">
       {initialLoad ? (
         <p className="text-center text-xs text-gray-300">Loading...</p>
       ) : (
