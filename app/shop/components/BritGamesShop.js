2054aac1ca9f60d64e180bb54b0c186d74ddb7f7
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-08-29 23:03:50 +0100

    starlink

--- app/shop/components/BritGamesShop.js
+++ app/shop/components/BritGamesShop.js
@@ -292,8 +292,8 @@ const players = ["Oddwin", "Lou", "Lad(Michael)", "Hermit(Michael)", "Lentil", "
         <div className="bg-gray-800/80 p-4 rounded-md shadow-md border-2 flex flex-col justify-between h-full">
           <h3 className="text-center text-xl font-semibold">Starlink</h3>
           <img
-            src="https://cdn.pixabay.com/photo/2013/07/13/12/19/dinghy-159624_1280.png"
-            alt="Safety Boat"
+            src="/images/starlink.png"
+            alt="Starlink"
             className="w-full h-40 object-contain"
           />
           <p className="text-center text-sm mb-2 mt-2">They NEED this...</p>
