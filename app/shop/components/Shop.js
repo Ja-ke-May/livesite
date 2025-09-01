99b65447d239ebf94cdb5f10f2f24ae0a816f0ee
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-09-01 22:53:53 +0100

    tokens

--- app/shop/components/Shop.js
+++ app/shop/components/Shop.js
@@ -251,7 +251,7 @@ else {
 
         
         {isLoggedIn && username && (
-        <div className="mt-4 w-full flex items-center justify-center">
+        <div className="hidden mt-4 w-full flex items-center justify-center">
           <button
             onClick={handleBuyTokens}
             className="bg-yellow-400 font-black brightness-125 text-[#000110] px-4 py-2 rounded-md shadow-sm hover:bg-yellow-600"
